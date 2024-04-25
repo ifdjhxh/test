3982c179e0380fc5d243551952af544f175f381b
Manucharova Angelina <90702206+ifdjhxh@users.noreply.github.com>
2024-04-25 17:34:43 +0300

    Update prompt_making.py

--- dir/src/task_condition_templates/prompt_making.py
+++ dir/src/task_condition_templates/prompt_making.py
@@ -1,4 +1,4 @@
 def question_making(question):
-    prompt_file = open("C:/Users/Acer/PycharmProjects/mse1h2024-task-gen/src/task_condition_templates/prompt.txt", 'r', encoding='utf-8')
+    prompt_file = open("task_condition_templates/prompt.txt", 'r', encoding='utf-8')
     text = prompt_file.readline()
     return text.replace('[mistake]', question)
